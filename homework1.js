aba7cb4f2ea5b3caa51bcfeb8ff880c3642bf362
FOLADA <128312088+FOLADA@users.noreply.github.com>
2024-07-18 03:42:26 -0700

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,9 @@
+function calculate(a, b) {
+    if (a == b) {
+        return "ტოლია"
+    } else {
+        return `არ არის ტოლი`
+    }
+}
+console.log(calculate(5, "5"))
+console.log(calculate(3, "4"))  
